46bb7f00567db986bc5d0d8254837a32e7bd57ae
afmssrahi <afmssrahi@gmail.com>
2021-11-16 19:18:11 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+	apiKey: 'AIzaSyCY-tWlmsNNyOnN01mP1U4c8dkm-Eqsjcs',
+	authDomain: 'ema-john-simple-12345.firebaseapp.com',
+	projectId: 'ema-john-simple-12345',
+	storageBucket: 'ema-john-simple-12345.appspot.com',
+	messagingSenderId: '724779272332',
+	appId: '1:724779272332:web:94f7029f3bab8a443993a9',
+};
+
+export default firebaseConfig;
---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import { clearTheCart, getStoredCart } from '../../utilities/fakedb';
+import './Shipping.css';
+
+const Shipping = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		reset,
+	} = useForm();
+	const { user } = useAuth();
+	const onSubmit = (data) => {
+		const saveCart = getStoredCart();
+		data.order = saveCart;
+
+		fetch('http://localhost:5000/orders', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				if (result.insertedId) {
+					alert('Order processed successfully');
+					clearTheCart();
+					reset();
+				}
+			});
+	};
+	return (
+		<div>
+			<form className='shipping-form' onSubmit={handleSubmit(onSubmit)}>
+				<input defaultValue={user.displayName} {...register('name')} />
+
+				<input
+					defaultValue={user.email}
+					{...register('email', { required: true })}
+				/>
+				{errors.email && (
+					<span className='error'>This field is required</span>
+				)}
+				<input
+					placeholder='Address'
+					defaultValue=''
+					{...register('address')}
+				/>
+				<input
+					placeholder='City'
+					defaultValue=''
+					{...register('city')}
+				/>
+				<input
+					placeholder='phone number'
+					defaultValue=''
+					{...register('phone')}
+				/>
+
+				<input type='submit' />
+			</form>
+		</div>
+	);
+};
+
+export default Shipping;
